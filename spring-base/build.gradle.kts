36a5cf88064b25ebc7328e12c1b89f0aa4425dbd
Jackson Bailey <jackson.robert.bailey@gmail.com>
2022-04-19 12:45:40 -0400

    wip: bom

--- spring-base/build.gradle.kts
+++ spring-base/build.gradle.kts
@@ -3,5 +3,14 @@ plugins {
 }
 
 allprojects {
+
     group = "com.example.greeter"
+
+apply(plugin = "io.spring.dependency-management")
+
+the<DependencyManagementExtension>().apply {
+	imports {
+		mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+	}
+}
 }
